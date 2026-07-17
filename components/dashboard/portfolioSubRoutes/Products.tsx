c154bf469315b25e26df88dc4ac737fd27ca01e1
PrimeFactor-Dev <crawlpaydev@gmail.com>
2026-07-17 21:24:57 +0100

    fix: use strict equality in portfolioSubRoutes/Products.tsx length check

--- components/dashboard/portfolioSubRoutes/Products.tsx
+++ components/dashboard/portfolioSubRoutes/Products.tsx
@@ -54,7 +54,7 @@ const Products = ({ data }: { data: ProductType[] }) => {
 
       <MyMarket />
 
-      {data?.length == 0 ? (
+      {data?.length === 0 ? (
         <h1 className="mt-8 flex h-full w-full items-center justify-center text-xl font-medium text-darkgreen md:text-2xl">
           You don&apos;t have a product yet
         </h1>
